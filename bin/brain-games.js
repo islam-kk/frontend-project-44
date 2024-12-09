32bff4f84ad89234712a19369706f693f3e74dd9
Islam.K <islam.kaziev.1999@gmail.com>
2024-12-09 11:02:27 +0300

    Step4. Fixed brain-games.js and cli.js according eslint reqs

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import { getRawInput } from "readline-sync";
-import getNameandGreet from "../src/cli.js";
+import getNameandGreet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 getNameandGreet();
